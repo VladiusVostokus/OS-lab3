9273c598c1c475f0845f4e67646d2f5f07ab2728
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-06 21:15:29 +0200

    Model of function to access page

--- Core/MMU.go
+++ Core/MMU.go
@@ -1,5 +1,14 @@
 package core
 
-type MMU struct {
+type MMU struct {}
 
+func (m *MMU) AccessPage(pageTable *PageTable, c *Core, idx int) {
+	if (pageTable.Entries[idx].P == false) {
+		//c.PageFault()
+	}
+	pageTable.Entries[idx].R = true
+	writeProb := random(0, 10)
+	if (writeProb >= 7) {
+		pageTable.Entries[idx].M = true
+	}
 }
