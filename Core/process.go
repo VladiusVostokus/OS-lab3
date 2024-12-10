88b7692bed54158aeb436d7393793b2c66b8f0a2
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-10 20:53:15 +0200

    Return random PTE index instead of PTE pointer

--- Core/process.go
+++ Core/process.go
@@ -6,14 +6,14 @@ type Process struct {
     WorkingSet WorkingSet
 }
 
-func (p *Process) GetPTE() *PTE {
+func (p *Process) GetPTEIndex() int {
     // 90 % із WorkingSet.Indexies , 10% - будь який номер з PageTable.PTE.lenth
     //probability := random(0, 10)
     //if (probability >= 9) {
-    //   index := p.WorkingSet.PageIndexies[0]
+    //    index := p.WorkingSet.PageIndexies[0]
     //    return p.PageTable.Entries[index]
     //}
     randIndex := random(0, len(p.PageTable.Entries))
-    return p.PageTable.Entries[randIndex] // some index, rand
+    return randIndex // some index, rand
 }
 
