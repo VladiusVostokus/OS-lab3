8efff72647008ea9a6b038fb9be163f99d5ab9c9
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-13 09:52:31 +0200

    Comment model of main loop for interaction with processes

--- main.go
+++ main.go
@@ -11,8 +11,27 @@ func main() {
 	c.CreateProcess()
 	proc := c.GetProcess()
 	c.GenerateWorkingSet(proc)
+	//for proc, _ := range c.
 	for i := 0; i < proc.NReq; i++ {
 		index := proc.GetPTEIndex()
 		mmu.AccessPage(proc.PageTable, c, index)
 	}
+	/*
+	сounter := 0
+	for proc, _ := range c.RunQ {
+	// прокручувати процеси із списку по колу
+	counter++
+	десь тут згенерувати новий робочий набір для процесу
+	if (counter % 10 == 0) {
+		
+	}
+		counter - кожну 10 ітерацію оновлює стату(скидає біти звернення R для частини сторінок, напр для наступних 10)
+		for i := 0; i < proc.NReq; i++ {
+			index := proc.GetPTEIndex()
+			mmu.AccessPage(proc.PageTable, c, index)
+		}
+	}
+	*/
+	// додати ф.-процес ядра для статистики - скадає біти звернення(R)
+	// 
 }
