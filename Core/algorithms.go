a7dff2f824df63f513b7dad81ef98421cf24dc88
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-15 11:41:02 +0200

    Initial logic of NRU algorithm

--- Core/algorithms.go
+++ Core/algorithms.go
@@ -18,7 +18,42 @@ func (r *RandomAlgorithm) ReplacePage(c *Core, page **PhysicalPage) {
 type NRUAlgorithm struct{}
 
 func (nru *NRUAlgorithm) ReplacePage(c *Core, page **PhysicalPage) {
-	index := Random(0, len(c.BusyPages))
+	class1 := make([]int,0)
+	class2 := make([]int,0)
+	class3 := make([]int,0)
+	for i, v := range c.BusyPages {
+		if (v.PTE.R == false && v.PTE.M == false) {
+			page = &c.BusyPages[i]
+			(*page).PTE.P = false
+			fmt.Println("Replace page", i)
+			return
+		} else {
+			if (v.PTE.R == false && v.PTE.M == true) {
+				class1 = append(class1, i)
+			} else {
+				if (v.PTE.R == true && v.PTE.M == false) {
+					class2 = append(class2, i)
+				} else {
+					if (v.PTE.R == true && v.PTE.M == true) {
+						class3 = append(class3, i)
+					}
+				}
+			}
+		}
+	}
+
+	var index int
+	if (len(class1) > 0) {
+		index = Random(0, len(class1))
+	} else {
+		if (len(class2) > 0) {
+			index = Random(0, len(class2))
+		} else {
+			if (len(class3) > 0) {
+				index = Random(0, len(class3))
+			}
+		}
+	}
 	page = &c.BusyPages[index]
 	(*page).PTE.P = false
 	fmt.Println("Replace page", index)
